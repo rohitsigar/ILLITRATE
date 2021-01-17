e4c755d165fdcaaef80096ce1ba08605fd0983e7
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-01-18 03:38:41 +0530

    blog

--- src/Components/BeadStory.js
+++ src/Components/BeadStory.js
@@ -1,21 +1,110 @@
 import React from "react";
 import "./BeadStory.css";
-import Lottie from 'react-lottie';
+import Lottie from "react-lottie";
 import diamond from "../Lottie/diamond.json";
 
 export default function BeadStory() {
-  return <div className="binary_search">
+  return (
+    <div className="binary_search">
       <div className="binary_heading">
-          <h1>
-            <span className="mystery">Mystery</span>
-            <div className="diamond">
-            <Lottie options={{ animationData:diamond,}} />
-            </div>
-            <span className="bead">Beads</span>
-          </h1>
+        <h1>
+          <span className="mystery">Mystery</span>
+          <div className="diamond">
+            <Lottie options={{ animationData: diamond }} />
+          </div>
+          <span className="bead">Beads</span>
+        </h1>
       </div>
-      <div className="binary_story">
-          
+      <div className="binary_story"></div>
+      <div className="binary__blog">
+        <h2>Binary Search</h2>
+        <p>
+          Binary Search Algorithm is one of the widely used searching techniques
+          on a sorted array. This searching technique follows the divide and
+          conquer strategy. The search space always reduces to half in every
+          iteration.
+        </p>
+        <video
+          className="home__logo"
+          autoPlay={true}
+          loop={true}
+          muted={true}
+          playsInline={true}
+        >
+          <source
+            src="https://github.com/rohitsigar/ILLITRATE/blob/master/WhatsApp%20Video%202021-01-18%20at%2000.40.12.mp4?raw=true"
+            type="video/webm"
+          />
+        </video>
+        <h4> Approach </h4>
+        <ul>
+          <li>
+            <p className="binary_step">
+              Array is supposed to be sorted in ascending order.
+            </p>
+          </li>
+          <li>
+            <p className="binary_step">
+              Firstly initialize the two pointers, start at 0 and end at the
+              last index of the array
+            </p>
+          </li>
+          <li>
+            <p className="binary_step">
+              Next, we will introduce another variable mid which will mark the
+              middle of the current array. That will be computed as
+              (low+high)/2.
+            </p>
+          </li>
+          <li>
+            <p className="binary_step">
+            If the element present at the mid index is equal to the element to be searched, then just return the mid index.
+            </p>
+          </li>
+          <li>
+            <p className="binary_step">
+            If the element to be searched is smaller than the element present at the mid index, move end to mid-1, and all RHS will get discarded.            </p>
+          </li>
+          <li>
+            <p className="binary_step">
+            If the element to be searched is greater than the element present at the mid index, move beg to mid+1, and all LHS will get discarded.            </p>
+          </li>
+        </ul>
+        <img
+          className="binary__code"
+          src="https://github.com/rohitsigar/ILLITRATE/blob/master/src/Assets/binarySearch%20Snippet.PNG?raw=true"
+          alt="/"
+        ></img>
+        {/* <p classname="binary_ques">
+          If there is a n sized array & you have to find element-x from the
+          array, then what is the average number of indexes you have to
+          traverse?
+          <span className="binary_think">Think about it?</span>
+        </p> */}
+        <h4> Time Complexity </h4>
+        <p classname="binary_timeCom">
+        With each iteration, the search space is getting divided by that means
+         that in the current iteration we have to deal with half of the previous iteration array, 
+         and this step will continue till start reaches the end. Best case is where we found the searched element
+          exactly at mid of array
+          <br></br>
+          <h3>Best: O(1) </h3>
+          <h3> Average: O(logn) </h3>
+          <h3> Worst: O(logn)</h3>
+        </p>
+        <h4> Why to prefer ?</h4>
+        <p className="binary_diff">
+        Since we always want to have less time complexity, and we know in linear searching, time is taken to search elements increases with an increase in the number of elements but in binary searching, it is done to either half of the original array. Also, Linear search performs
+         equality comparisons and Binary search performs ordering comparisons.
+        </p>
+
+        <h6 className="binary__con">
+          Concluding above article, we get to know that is a quick way to 
+          find a value in a sorted sequesnce. Binary search maintains a contiguous subsequence
+          of the starting sequence where the target value is surely located i.e. the search space.
+        </h6>
       </div>
-  </div>;
+    </div>
+  );
 }
+
