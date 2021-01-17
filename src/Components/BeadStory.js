063f6b5de7bf1d7f5251887f9e37f19bf12d21a9
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-01-17 22:01:15 +0530

    bead

---
+++ src/Components/BeadStory.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function BeadStory() {
+  return <div className="binary_search">
+      <div className="binary_heading">
+          <h1>
+
+          </h1>
+      </div>
+      <div className="binary_story">
+          
+      </div>
+  </div>;
+}
